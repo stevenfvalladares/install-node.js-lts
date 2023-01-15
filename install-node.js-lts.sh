ef087023c9bd8ddb7a2f81edf408ecf344ed10af
Steven Valladares <stevenfvalladares@gmail.com>
2023-01-15 11:29:14 -0300

    Developing the script to run the installation

---
+++ install-node.js-lts.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# remove old version of node.js
+sudo apt remove nodejs -y
+# update repositories
+sudo apt update && sudo apt upgrade -y
+# curl installation
+sudo apt install curl
+# nvm installation
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# add command to reset terminal without interrupting installation
+
+# installation of node.js in its stable version
+nvm install --lts
+# check installation
+node -v
+
