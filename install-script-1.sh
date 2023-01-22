9d8f783096707bae6c4525b3afba51136f907095
Steven Valladares <stevenfvalladares@gmail.com>
2023-01-21 21:35:08 -0300

    Script 1 lines are updated

--- install-script-1.sh
+++ install-script-1.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 
-# update repositories
+# Update repositories
 sudo apt update && sudo apt upgrade -y
-# curl installation
+# CURL installation
 sudo apt install curl
-# nvm installation
+# NVM installation
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-# add command to reset terminal without interrupting installation
-
-# installation of node.js in its stable version
-nvm install --lts
-# check installation
-node -v
-
+# To finish the installation, you must restart the system, open a new command line and execute script 2.
+reboot
